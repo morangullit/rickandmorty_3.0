120475f5585b71773fc1902285e756a852da54a5
Gullit Moran <gullitmoran@gmail.com>
2023-03-02 18:58:09 -0500

    Arrglando codigo de Form

--- front/src/components/Form/Form.jsx
+++ front/src/components/Form/Form.jsx
@@ -23,10 +23,22 @@ export const Form = ({login}) => {
       <h1 className={styles.login}>Iniciar Sesión</h1>
         <form onSubmit={handleSubmit}>
             <label className={styles.label} htmlFor="username">Username:</label>
-            <input type="text" name="username" placeholder="Ingresa tu usuario" className={styles.input} value={userData.username} onChange={handleInputChange} />
+            <input  type="text" 
+                    name="username" 
+                    placeholder="Ingresa tu usuario" 
+                    className={styles.input} 
+                    value={userData.username} 
+                    onChange={handleInputChange}
+             />
             <p className={styles.err}>{errors.username}</p>
             <label className={styles.label} htmlFor="password">Password:</label>
-            <input type="password" name="password" placeholder="Ingresa tu contraseña" className={styles.input} value={userData.password} onChange={handleInputChange} />
+            <input  type="password" 
+                    name="password" 
+                    placeholder="Ingresa tu contraseña" 
+                    className={styles.input} 
+                    value={userData.password} 
+                    onChange={handleInputChange} 
+            />
             <p className={styles.err}>{errors.password}</p>
             <br/>
             <button className={styles.button} type="submit" >Ingresar</button>
